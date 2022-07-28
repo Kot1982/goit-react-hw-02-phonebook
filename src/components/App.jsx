e3b6b582cc99322ef7bb981f730a487640042efe
Kostiantyn Oliinyk <94046694+Kot1982@users.noreply.github.com>
2022-07-28 06:33:20 +0300

    start task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleNameChange = event => {
+    // console.log(event.currentTarget.value)
+    this.setState({name: event.currentTarget.value})
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Phonebook</h1>
+          <form>
+            Name
+            <label>
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleNameChange}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              ></input>
+            </label>
+            <button type='submit'>Add contact</button>
+          </form>
+          <h2>Contacts</h2>
+        </div>
+      </>
+    );
+  }
+}
+export default App;
